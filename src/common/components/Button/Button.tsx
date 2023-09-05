3bf758ef969b439b3a7022f88c41e7b9513b6202
kiselevMaksim1223 <laurkurt@gmail.com>
2023-09-05 18:52:00 +0400

    feat: common components added

---
+++ src/common/components/Button/Button.tsx
@@ -0,0 +1,17 @@
+import React, {FC, ReactNode} from 'react';
+
+interface IButton {
+  children?: ReactNode
+  onClickCallback: () => void
+  className?: string
+}
+export const Button:FC<IButton> = ({children, onClickCallback, className}) => {
+
+  const onClickHandler = () => {
+    onClickCallback()
+  }
+
+  return (
+    <button className={`px-2 bg-blue-400 rounded hover:bg-blue-600 font-light ${className}`} onClick={onClickHandler}>{children}</button>
+  );
+};
---
+++ src/common/components/addItem/AddItem.tsx
@@ -0,0 +1,22 @@
+import React, {FC, useState} from 'react';
+import { Button } from "../Button/Button";
+
+interface IAddItem {
+  name: string
+  onClickCallback: (title: string) => void
+}
+export const AddItem:FC<IAddItem> = ({onClickCallback, name}) => {
+  const [title, setTitle] = useState<string>('')
+
+  const onClickHandler = () => {
+    title.trim() !== '' && onClickCallback(title)
+    setTitle('')
+  }
+
+  return (
+    <div className={"flex gap-2 justify-between"}>
+      <input placeholder={'Title'} value={title} className={"w-7/12 grow rounded-md h-10 border-2 border-gray-300 p-1"} onChange={(e) => setTitle(e.currentTarget.value)} />
+      <Button  onClickCallback={onClickHandler}>{name}</Button>
+    </div>
+  );
+};
