572048d9c59d8bcbf21a1faa89e69d4a67785cab
kiselevMaksim1223 <laurkurt@gmail.com>
2023-09-05 18:49:42 +0400

    react app init

---
+++ src/App/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {Todolists} from "../components/Todolists/Todolists";
+
+export const App = () => {
+  return (
+      <Todolists/>
+  );
+}
---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'
+import reportWebVitals from './reportWebVitals';
+import {App} from "./App/App";
+import {Provider} from "react-redux";
+import {store} from "./store/createStore";
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+reportWebVitals();
