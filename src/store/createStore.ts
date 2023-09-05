f1739cb0495363de2f4a292f69e087c8ce388d6e
kiselevMaksim1223 <laurkurt@gmail.com>
2023-09-05 18:51:18 +0400

    store added

---
+++ src/store/createStore.ts
@@ -0,0 +1,24 @@
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import { todolistsReducer } from "./reducers/todolistsSlice/slice";
+import {tasksReducer} from "./reducers/tasksSlice/slice";
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const rootReducer = combineReducers({
+    todolists: todolistsReducer,
+    tasks: tasksReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer
+})
+
+export type RootStateType = ReturnType<typeof store.getState>;
+export type AppDispatchType = typeof store.dispatch;
